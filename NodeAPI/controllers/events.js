f1cd47d7a8cafa0afb6a9b3e59b1f08403bd48a1
Michael <wuxueyuan10@gmail.com>
2021-08-16 12:46:38 +1000

    fix api issues

--- NodeAPI/controllers/events.js
+++ NodeAPI/controllers/events.js
@@ -17,27 +17,26 @@ exports.list = async (req, res, next) => {
 
         if (data && data.events) {
             for (i = 0; i < data.events.length; i++) {
-                if (i === 3) {
-                    break;
-                }
                 if (data.events[i].status === "completed") {
-                    break;
-                } else {
-                    events.push({
-                        "id": "00" + (i + 1),
-                        "name": data.events[i].name.text,
-                        "url": data.events[i].url,
-                        "start": utils.formatDate(data.events[i].start.local),
-                        "duration": utils.getDuaration(data.events[i].start.local, data.events[i].end.local) + " hours",
-                        "img": data.events[i].logo.original.url,
-                        "capacity": data.events[i].capacity,
-                        "location": await getVenueAddress(data.events[i].venue_id),
-                        "status": data.events[i].status
-                    })
+                    continue;
                 }
 
+                const location = await getVenueAddress(data.events[i].venue_id)
+
+                events.push({
+                    "id": "00" + (i + 1),
+                    "name": data.events[i].name.text,
+                    "url": data.events[i].url,
+                    "start": utils.formatDate(data.events[i].start.local),
+                    "duration": utils.getDuaration(data.events[i].start.local, data.events[i].end.local) + " hours",
+                    "img": data.events[i].logo.original.url,
+                    "capacity": data.events[i].capacity,
+                    "location": location,
+                    "status": data.events[i].status
+                })
             }
         }
+
         res.status(200).json(events);
     } catch (err) {
         next(err);
@@ -57,17 +56,22 @@ async function getOrganisationId() {
 }
 
 async function getVenueAddress(id) {
-    const customUrl = baseUrl + `/venues/${id}`;
-    let address = '';
-    const response = await axios.get(customUrl, {
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    });
+    if (id) {
+        const customUrl = baseUrl + `/venues/${id}`;
+        let address = '';
+        const response = await axios.get(customUrl, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
 
-    if (response) {
-        const data = response.data.address;
-        address = data.address_1 + ", " + data.city + " " + data.region + " " + data.postal_code;
-        return address;
+        if (response) {
+            const data = response.data.address;
+            address = data.address_1 + ", " + data.city + " " + data.region + " " + data.postal_code;
+            return address;
+        }
+
+    } else {
+        return "Online Zoom Meeting"
     }
 }
